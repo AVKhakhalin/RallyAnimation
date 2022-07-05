8fca0b55dbcc24952b9de89c1178baa155af07ec
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-07-05 18:46:58 +0300

    Реализация движения машинки по клику на неё. Так же реализовано движение машинки по произвольно начерченному на экране пути.

--- app/src/main/java/com/car/formula/animation/rally/view/MainActivity.kt
+++ app/src/main/java/com/car/formula/animation/rally/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.car.formula.animation.rally
+package com.car.formula.animation.rally.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.car.formula.animation.rally.R
 
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
