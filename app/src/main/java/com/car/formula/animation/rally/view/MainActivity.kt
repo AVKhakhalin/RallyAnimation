b474e18b0ee9bc91b949e698e5248081e145d825
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-07-05 19:06:46 +0300

    Добавлен огонь на сцене для красоты (хотя это приводит к большим "лагам" при отображении сцены). Его можно включить или выключить кнопкой.

--- app/src/main/java/com/car/formula/animation/rally/view/MainActivity.kt
+++ app/src/main/java/com/car/formula/animation/rally/view/MainActivity.kt
@@ -2,11 +2,28 @@ package com.car.formula.animation.rally.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.car.formula.animation.rally.R
+import com.car.formula.animation.rally.SceneView.FireView
 
 class MainActivity: AppCompatActivity() {
+    /** Исходные данные */ //region
+    private lateinit var buttonFireOnOff: Button
+    private var isFireOn: Boolean = false
+    private lateinit var fireWall: FireView
+    //endregion
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonFireOnOff = findViewById(R.id.fire_on_off)
+        fireWall = findViewById(R.id.firewall)
+        buttonFireOnOff.setOnClickListener {
+            isFireOn = !isFireOn
+            if (isFireOn) fireWall.visibility = View.VISIBLE
+            else fireWall.visibility = View.INVISIBLE
+        }
     }
 }
